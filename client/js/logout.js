db4c4defe8328b9acafe465b2e9fd4c9ee5779f3
Clarence Cho <clarencemindpalace@gmail.com>
2024-04-26 21:25:42 +0800

    Update

---
+++ client/js/logout.js
@@ -0,0 +1,15 @@
+// const logoutBtn = document.getElementById("logoutButton");
+
+// logoutBtn.addEventListener("click", async () => {
+//   try {
+//     const response = await fetch("/logout", { method: "POST" });
+//     if (response.ok) {
+//       // If the logout was successful, redirect to the index page
+//       window.location.href = "index.html";
+//     } else {
+//       console.error("Logout failed");
+//     }
+//   } catch (error) {
+//     console.error("Error:", error);
+//   }
+// });
