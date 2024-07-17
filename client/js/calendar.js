c104a3dfe9de12dd01c30d7fd9ca214d869367e2
Clarence Cho <clarencemindpalace@gmail.com>
2024-07-17 20:38:00 +0800

    Update

--- client/js/calendar.js
+++ client/js/calendar.js
@@ -1,59 +1,150 @@
-const monthYearElement = document.getElementById('monthYear');
-const datesElement = document.getElementById('dates');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
+const calendar = document.querySelector(".calendar");
+const date = document.querySelector(".date");
+const daysContainer = document.querySelector(".days");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const todayBtn = document.querySelector(".today-btn");
+const gotoBtn = document.querySelector(".goto-btn");
+const dateInput = document.querySelector(".date-input");
 
-let currentDate = new Date();
+// ========== GETTING THE CURRENT DATE ==========
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
 
-const updateCalendar = () => {
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
+// ======= ARRAYS OF MONTHS AND DAYS ==========
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-    const firstDay = new Date(currentYear, currentMonth,0);
-    const lastDay = new Date(currentYear, currentMonth + 1, 0);
-    const totalDays = lastDay.getDate();
-    const firstDayIndex = firstDay.getDay();
-    const lastDayIndex = lastDay.getDay();
+// =========== FUNCTION TO ADD DAYS ============
 
-    const monthYearString = currentDate.toLocaleString('default', {month: 'long', year:'numeric'});
-    monthYearElement.textContent = monthYearString;
+function initCalendar() {
+  // TO GET PREV MONTH DAYS AND CURRENT MONTH ALL DAYS AND REM NEXT MONTH DAYS
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
 
-    let datesHTML = '';
+  // UPDATE DATE TOP OF CALENDER
+  date.innerHTML = months[month] + " " + year;
 
-    for(let i = firstDayIndex; i > 0; i--){
-        const prevDate = new Date(currentYear,currentMonth, 0 - i + 1);
-        datesHTML += `<div class= "date inactive">${prevDate.getDate()}</div>`;
+  // ADDING DAYS ON DOM
+  let days = "";
+
+  // PREVIOUS MONTH DAYS
+
+  for (let x = day; x > 0; x--) {
+    days = `<div class="day prev-date">${prevDays - x + 1}</div>` + days;
+  }
+
+  // CURRENT MONTH DAYS
+  for (let i = 1; i <= lastDate; i++) {
+    // IF DAY IS TODAY ADD CLASS TODAY
+    if (
+      i === new Date().getDate() &&
+      year === new Date().getFullYear() &&
+      month === new Date().getMonth()
+    ) {
+      days += `<div class="day today">${i}</div>`;
     }
-
-    for(let i = 1; i <=totalDays; i++){
-        const date = new Date(currentYear, currentMonth, i);
-        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
-        datesHTML += `<div class="date ${activeClass}">${i}</div>`;
+    // ADD REMAINING AS IT IS
+    else {
+      days += `<div class="day">${i}</div>`;
     }
+  }
 
-    for(let i = 1; i <= 7 - lastDayIndex; i++){
-        const nextDate = new Date(currentYear, currentMonth + 1, i);
-        datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
-    }
+  // NEXT MONTH DAYS
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next-date">${j}</div>`;
+  }
 
-    datesElement.innerHTML = datesHTML;
+  daysContainer.innerHTML = days;
+}
+initCalendar();
+
+function prevMonth() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+
+  initCalendar();
 }
 
-prevBtn.addEventListener('click', () => {
-    currentDate.setMonth(currentDate.getMonth() - 1);
-    updateCalendar();
-})
+// NEXT MONTH
+function nextMonth() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  initCalendar();
+}
 
-nextBtn.addEventListener('click', () => {
-    currentDate.setMonth(currentDate.getMonth() + 1);
-    updateCalendar();
-})
+// ADD EVENLISTENERS ON PREV AND NEXT BUTTONS
 
-updateCalendar();
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
 
-// ============== POP UP FOR CALENDAR =================
+// =========== GO TO DAYS ============
 
-const plusIconContainer = document.getElementById('plusIconContainer'); 
-const plusIconFunctionality = document.getElementById('plusIconFunctionality');
-const addEvent =  document.getElementById('addEvent');
-    
+todayBtn.addEventListener("click", () => {
+  today = new Date();
+  month = today.getMonth();
+  year = today.getFullYear();
+  initCalendar();
+});
+
+dateInput.addEventListener("change", (e) => {
+  dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+  if (dateInput.value.length === 2) {
+    // ADD A SLASH IF TWO NUMBERS ENTERED
+    dateInput.value += "/";
+  }
+  if (dateInput.value.length > 7) {
+    // DONT ALLOW MORE THAN 7 CHARACTER
+    dateInput.value = dateInput.value.slice(0, 7);
+  }
+
+  // REMOVING SLASHES IF BACKSPACE PRESSED
+  if ((e.inputType = "deleteContentBackward")) {
+    if (dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0, 2);
+    }
+  }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+
+function gotoDate() {
+  const dateArr = dateInput.value.split("/");
+  // DATE VALIDATION
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+
+  alert("Data Format Must Be : (MM/YYYY)");
+}
